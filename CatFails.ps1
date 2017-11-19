5d8dba56a72ccbcba699614ce04e2e4bba208104
Tomas Hellman <tomas.hellman@telecomputing.se>
2017-11-19 15:51:06 +0100

    commit på catfails, nu med starwars

--- CatFails.ps1
+++ CatFails.ps1
@@ -1,3 +1,12 @@
+# Parameter help description
+Param 
+(
+    [Parameter(Mandatory=$true)]
+    [ValidateSet("CatFails","imperialmarch")]
+    [String[]]
+    $Action
+)
+ 
  function PlayCatFails
  {
    $Shellobject = New-Object -ComObject shell.application
@@ -7,4 +16,34 @@
     $IEObject.FullScreen=$true
     $IEObject.navigate2("https://www.youtube.com/tv#/watch?v=79EvGkXHF9I") 
  }
+ function imperialmarch
+ {
+ 
+     [console]::beep(440,500)       
+     [console]::beep(440,500) 
+     [console]::beep(440,500)        
+     [console]::beep(349,350)        
+     [console]::beep(523,150)        
+     [console]::beep(440,500)        
+     [console]::beep(349,350)        
+     [console]::beep(523,150)        
+     [console]::beep(440,1000) 
+     [console]::beep(659,500)        
+     [console]::beep(659,500)        
+     [console]::beep(659,500)        
+     [console]::beep(698,350)        
+     [console]::beep(523,150)        
+     [console]::beep(415,500)        
+     [console]::beep(349,350)        
+     [console]::beep(523,150)        
+     [console]::beep(440,1000)    
+ }
+ 
+if ($action -eq 'imperialmarch')
+{
+ imperialmarch   
+}
+if ($action -eq 'catfails')
+{
  PlayCatFails
+}
