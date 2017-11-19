8d2bbc8181986d18e41701371622586f5946a466
Simon Wahlin <simon.wahlin@knowledgefactory.se>
2017-11-19 12:27:53 +0100

    Added tests for SMB1 Disabled

--- ClientTests/CoreClient.Tests.ps1
+++ ClientTests/CoreClient.Tests.ps1
@@ -1,3 +1,5 @@
+$IsElevated = [System.Security.Principal.WindowsPrincipal][System.Security.Principal.WindowsIdentity]::GetCurrent() | 
+    Foreach-Object -MemberName IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
 Describe 'Core client configuration tests' {
     Context 'Connectivity' {
         It 'Has network connectivity' {
@@ -17,6 +19,17 @@ Describe 'Core client configuration tests' {
             Test-NetConnection -CommonTCPPort HTTP |
                 Select-Object -ExpandProperty TcpTestSucceeded |
                 Should -Be $true
+        }
+    }
+
+    Context 'Features' {
+        if($true -eq $IsElevated) {
+            $SMB1Features = Get-WindowsOptionalFeature -FeatureName SMB1* -Online
+            foreach($Feature in $SMB1Features) {
+                It "Has $($Feature.FeatureName) DISABLED" {
+                    $Feature.State | Should -Be 'Disabled'
+                }
+            }   
         }
     }
 }
